695573b354f6ec71c7d00e6f1f1c552352d731ad
echendu Chinedum <echendu.chinedum@gmail.com>
2021-05-12 15:33:30 +0100

    refactored for cors error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 require("dotenv").config();
 const UserRoute = require("./routes/user");
 require("./db/mongoose");
@@ -8,7 +8,13 @@ const port = process.env.PORT || 4000;
 const app = express();
 
 // serving up the user route json middleware
-app.use(cors())
+app.use(cors());
+// second fix to the cors errors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(express.json());
 app.use(UserRoute);
 
